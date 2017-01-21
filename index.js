c5d81f98996ce8b78f9c5c743e278735077a09d0
JSProto <jake@mbpr.local>
2017-01-21 18:57:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+    airmon: require('./lib/airmon-ng'),
+    airdump: require('./lib/airodump-ng'),
+    airplay: require('./lib/aireplay-ng'),
+    aircrack: require('./lib/aircrack-ng')
+};
